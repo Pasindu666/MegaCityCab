a7df75ab51008840bafd8ce5dfc732af2e092c71
Pasindu Nimsara <pasindu05nimsara@gmail.com>
2025-03-09 09:47:33 +0530

    Security config

--- src/main/java/com/pasindu/MegaCityCab/service/CustomUserDetailsService.java
+++ src/main/java/com/pasindu/MegaCityCab/service/CustomUserDetailsService.java
@@ -6,13 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByEmail(username).orElseThrow(()->new OurException("User Not Found"));
+        return userRepository.findByEmail(username).orElseThrow(()->new OurException("Username/Email Not Found"));
     }
 }
